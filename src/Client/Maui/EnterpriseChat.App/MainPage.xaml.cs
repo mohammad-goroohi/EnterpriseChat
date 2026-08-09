52d8034ea4a4775a64c53d2c0687caf8cd3f84f1
goroohi-m <mohammad.goroohi0582@gmail.com>
2026-08-09 21:29:29 +0330

    init repo

---
+++ src/Client/Maui/EnterpriseChat.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EnterpriseChat.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
